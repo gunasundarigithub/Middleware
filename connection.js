bdcf359d1b5dbc2ef9c79d1a1ac572a271177aa3
gunasundarigithub <divyaeee02@gmail.com>
2020-08-20 23:57:38 -0700

    added foreign key and tested in connection.js

--- connection.js
+++ connection.js
@@ -3,20 +3,29 @@
 const ADODB = require('node-adodb');
 const connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Shift_Scheduler.mdb;');
  
-connection
-  .execute('INSERT INTO Team(Team_id, Team_name) VALUES (8,"Anna")')
-  .then(data => {
-    console.log(JSON.stringify(data, null, 2));
-  })
-  .catch(error => {
-    console.error(error);
-  });
+// connection
+//   .execute('INSERT INTO Team(Team_id, Team_name) VALUES (8,"Anna")')
+//   .then(data => {
+//     console.log(JSON.stringify(data, null, 2));
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
   connection
-  .query('SELECT * FROM Team')
+  .query('SELECT * FROM Employee INNER JOIN Team ON Team.Team_id=Employee.Team_id')
   .then(data => {
-    console.log(JSON.stringify(data, null, 2));
+    console.log(JSON.stringify(data));
   })
   .catch(error => {
     console.error(error);
   });
+
+  // connection
+  // .query('SELECT * FROM Shift')
+  // .then(data => {
+  //   console.log(JSON.stringify(data));
+  // })
+  // .catch(error => {
+  //   console.error(error);
+  // });
  

4475ef6342aea8198af8c28b89cc7aece3ba033d
gunasundarigithub <divyaeee02@gmail.com>
2020-09-02 23:24:09 -0700

    pushing on 09/02

--- connection.js
+++ connection.js
@@ -145,11 +145,14 @@ app.get('/getemployee',(req,res)=>{
 
   console.log(req.body)
 
+  //INSERT INTO Calender VALUES (${req.body.Month},${req.body.Month_number},${req.body.year},${req.body.team_id},${req.body.Employee_name})
   //INSERT INTO Calender VALUES('August',2020,1,'Divya',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)
-    connection
-  .execute(`INSERT INTO Calender VALUES (${req.body.Month},${req.body.Month_number},${req.body.year},${req.body.team_id},${req.body.Employee_name})`)
+    connection.execute(`INSERT INTO Calender VALUES (${req.body.Month},${req.body.Month_number},${req.body.year},${req.body.team_id},${req.body.Employee_name},${req.body.Day1},${req.body.Day2},${req.body.Day3},${req.body.Day4},${req.body.Day5},
+      ${req.body.Day6},${req.body.Day7},${req.body.Day8},${req.body.Day9},${req.body.Day10},${req.body.Day11},${req.body.Day12},${req.body.Day13},${req.body.Day14},${req.body.Day15},${req.body.Day16},${req.body.Day17},${req.body.Day18},${req.body.Day19}
+      ,${req.body.Day20},${req.body.Day21},${req.body.Day22},${req.body.Day23},${req.body.Day24},${req.body.Day25},${req.body.Day26},${req.body.Day27},${req.body.Day28},${req.body.Day29},${req.body.Day30},${req.body.Day31})`)
   .then(data => {
     console.log(JSON.stringify(data, null, 2));
+    res.send("data inserted sucessfully")
   })
   .catch(error => {
     console.error(error);
@@ -160,25 +163,10 @@ app.get('/getemployee',(req,res)=>{
       Year_num:req.query.year,
       Team_id:req.query.team_id,
       Employee_name:req.query.Employee_name,
-      // Day2,Day3,Day4,Day5,Day6,Day7,Day8,Day9,Day10,
-      // Day11,Day12,Day13,Day14,Day15,Day16,Day17,Day18,Day19,Day20,
-      // Day21,Day23,Day23,Day24,Day25,Day26,Day27,Day28,Day29,Day30}
+      Day1:req.query.shift1
 
      };
 
-    //  let insert_data= `INSERT INTO Calender (Month_name,Year_num,Team_id,Employee_name) VALUES (${req.body.Month},${req.body.year},${req.body.team_id},${req.body.Employee_name})`;
-    // console.log(insert_data)
-    //  connection.query(insert_data).then(data => {
-    //   // console.log(JSON.stringify(data));
-    //    //const result = JSON.stringify(data);
-    //    console.log(data)
-
-    //    res.send(data)
-    //  })
-    //  .catch(error => {
-    //    console.error(error);
-    //  });
-    
  });
 
 
